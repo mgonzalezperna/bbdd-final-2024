c45d7c7ec828884b6facf75e9c798b7695a6cb27
mgonzalezperna <mgonzalezperna@gmail.com>
2024-01-28 03:02:14 +0100

    dockerize project

--- backend/backend/server.py
+++ backend/backend/server.py
@@ -10,7 +10,7 @@ import logging
 app = Flask(__name__)
 cors = CORS(app)
 
-app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_HOST'] = 'mysql'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'admin'
 app.config['MYSQL_DB'] = 'BBDD'
